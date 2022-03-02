5c60b4f4abeb495e8e63906252ed65b41ab95e4c
wukongO-O <l.shenglan@gmail.com>
2022-03-02 14:53:07 -0500

    completed basic game setup

--- game-rps.js
+++ game-rps.js
@@ -3,32 +3,60 @@ function computerPlay() {
     let result = play[Math.floor(Math.random()* 3)];
     return result;
 }
-computerPlay();
-console.log(computerPlay());
-
-const computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) { 
+    playerSelection = prompt("Pick 1 of the followings: rock, paper, scissors");
+    computerSelection = computerPlay();
     if (computerSelection === 'Paper' && playerSelection.toLowerCase() === 'rock') {
-        return ('You Lose! Paper beats Rock');
+        return (-1);
     } else if (computerSelection === 'Paper' && playerSelection.toLowerCase() === 'paper') {
-        return ('Tie!');
+        return (0);
     } else if (computerSelection === 'Paper'&& playerSelection.toLowerCase() === 'scissors') {
-        return ('You Win! Scissors beats Paper');
+        return (1);
     } else if (computerSelection === 'Rock' && playerSelection.toLowerCase() === 'rock') {
-        return ('Tie!'); 
+        return (0); 
     } else if (computerSelection === 'Rock' && playerSelection.toLowerCase() === 'paper') {
-        return ('You Win! Paper beats Rock');
+        return (1);
     } else if (computerSelection === 'Rock' && playerSelection.toLowerCase() === 'scissors') {
-        return ('You Lose! Rock beats Scissors');
+        return (-1);
     } else if (computerSelection === 'Scissors' && playerSelection.toLowerCase() === 'rock') {
-        return ('You Win! Rock beats Scissors');
+        return (1);
     } else if (computerSelection === 'Scissors' && playerSelection.toLowerCase() === 'paper') {
-        return ('You Lose! Scissors beats Paper');
-    } else if (computerSelection === 'Scissors' && playerSelection.toLowerCase() === 'scissors') {
-        return ('Tie!');    
+        return (-1);
+    } else if (computerSelection === 'Scissors' && playerSelection.toLowerCase() === 'scissors'){
+        return (0);    
+    } else {
+        alert('I do not understand.')
     }
 }
 
-const playerSelection = 'pAper';
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let yourScore = 0;
+    let pcScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        if (result === 1) {
+            yourScore += 1;
+            pcScore += 0;
+        } else if (result === -1) {
+            yourScore += 0;
+            pcScore += 1;
+        } else if (result === 0) {
+            yourScore +=0;
+            pcScore += 0;
+        } else {
+            alert('invalid');
+        }
+
+        console.log(`You scored ${yourScore} : computer ${pcScore}`);
+    }
+    
+    if (yourScore > pcScore) {
+        return (`You won! You scored ${yourScore} : computer ${pcScore}`);
+    } else if (yourScore < pcScore) {
+        return (`You lost! You scored ${yourScore} : computer ${pcScore}`);
+    } else {
+        return (`It's a tie! You scored ${yourScore} : computer ${pcScore}`);
+    }
+}
+console.log(game());
